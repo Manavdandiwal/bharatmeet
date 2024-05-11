4eb7f82fc70de420fdeeacf6eec50bbed4957301
Manavdandiwal <73937633+Manavdandiwal@users.noreply.github.com>
2024-05-11 13:26:27 +0000

    initial commit

---
+++ app/(root)/layout.js
@@ -0,0 +1,7 @@
+import React, { Children } from "react";
+
+const RootLayout = ({ children }) => {
+  return <main>{children}</main>;
+};
+
+export default RootLayout;
---
+++ constants/index.js
@@ -0,0 +1,23 @@
+export const SidebarLinks = [
+    {
+        label: "Home",
+        imgUrl: "/icons/home.svg",
+        route: "/"
+    },
+    {
+        label: "Upcoming Meetings",
+        imgUrl: "/icons/upcoming.svg",
+        route: "/upcoming"
+    },
+    {
+        label: "Previous Meetings",
+        imgUrl: "/icons/previous.svg",
+        route: "/previous"
+    },
+    {
+        label: "Personal Room",
+        imgUrl: "/icons/personal.svg",
+        route: "/personal"
+    },
+
+]
