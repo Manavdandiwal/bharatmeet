d6b9896b55f2129193bed3583ce7de78fa361dc4
Manavdandiwal <73937633+Manavdandiwal@users.noreply.github.com>
2024-05-19 16:19:19 +0530

    testing middleware

--- middleware.js
+++ middleware.js
@@ -2,21 +2,21 @@ import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
 export default withAuth(
-    async function middleware(req) {
-        if (req.nextUrl.pathname == "/") {
-            return;
-        } else if (req.nextUrl.pathname.startsWith("/admin")) {
-            if (req.nextauth.token.role != "admin") {
-                return NextResponse.error("NOT AUTHORIZED");
-            }
-        } else {
-            if (req.nextauth.token.role == "admin") {
-                if (req.nextUrl.pathname === "/")
-                    return NextResponse.redirect("/admin");
-                return NextResponse.error("NOT AUTHORIZED");
-            }
-        }
-    },
+    // async function middleware(req) {
+    //     if (req.nextUrl.pathname == "/") {
+    //         return;
+    //     } else if (req.nextUrl.pathname.startsWith("/admin")) {
+    //         if (req.nextauth.token.role != "admin") {
+    //             return NextResponse.error("NOT AUTHORIZED");
+    //         }
+    //     } else {
+    //         if (req.nextauth.token.role == "admin") {
+    //             if (req.nextUrl.pathname === "/")
+    //                 return NextResponse.redirect("/admin");
+    //             return NextResponse.error("NOT AUTHORIZED");
+    //         }
+    //     }
+    // },
     {
         pages: {
             signIn: "/signin",
